93b4595c0cef79d5e70fe15c52e6c84e0f72830a
Billy Eskein <billyeskein@gmail.com>
2024-12-28 07:13:18 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solution here
+function findMatching(searchString,drivers) {
+const lowerCaseSearch = searchString.toLowerCase();
+return drivers.filter(driver =>driver.toLowerCase().includes(lowerCaseSearch));
+}// Code your solution here
+function findMatching(drivers, searchString) {
+    const lowerCaseSearchString = searchString.toLowerCase();
+    return drivers.filter(driver => driver.toLowerCase() === lowerCaseSearchString);
+}
+function fuzzyMatch(drivers, searchString) {
+    const lowerCaseSearchString = searchString.toLowerCase();
+    return drivers.filter(driver => driver.toLowerCase().startsWith(lowerCaseSearchString));
+}
+function matchName(drivers, nameToMatch) {
+    const lowerCaseNameToMatch = nameToMatch.toLowerCase();
+    return drivers.filter(driver => driver.name.toLowerCase() === lowerCaseNameToMatch);
+}
+
+
+const matchedDrivers = matchName(drivers, 'Alice');
+console.log(matchedDrivers);
+
+
